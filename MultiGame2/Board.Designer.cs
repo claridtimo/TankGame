088456f0b2d6f51107cb91c59c4ae87dadddf8a9
claridtimo <claridge.tim@gmail.com>
2014-09-26 17:38:48 +1200

    Added basic gui

---
+++ MultiGame2/Board.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows.Forms;
+
+namespace MultiGame2
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private bool running = true;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.MouseDown += new MouseEventHandler(this.Form_MouseDown);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(928, 522);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 546);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Board";
+            this.Text = "Board";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            //this.OnFormClosing(OnClosing();
+            this.FormClosed += new FormClosedEventHandler(onClose);
+
+            //MouseListener
+
+            
+
+        }
+
+        #endregion
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "Left Button Click"+e.X);
+                    break;
+                case MouseButtons.Right:
+                    MessageBox.Show(this, "Right Button Click");
+                    break;
+                case MouseButtons.Middle:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void onClose(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
---
+++ MultiGame2/PlayGame.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MultiGame2
+{
+    partial class PlayGame
+    {
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private Board b;
+        private TextBox textBox2;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.AccessibleDescription = "startButton";
+            this.startButton.AccessibleName = "sButton";
+            this.startButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Red;
+            this.startButton.Location = new System.Drawing.Point(517, 288);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(166, 76);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(517, 242);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(536, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(489, 465);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(244, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // PlayGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 602);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.startButton);
+            this.Name = "PlayGame";
+            this.Text = "selectPlayers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            
+
+            
+
+        }
+
+        #endregion
+
+
+        private void startClick(object sender, System.EventArgs e)
+        {
+            string theText = this.textBox1.Text;
+
+            //Console.WriteLine("TextSize " + this.textBox1.Text.Trim().Length);
+            if (this.textBox1.Text.Trim().Length != 0)
+            {
+                textBox2.AppendText("PlayerName: " + this.textBox1.Text);
+                b = new Board();
+                b.Show();
+            
+            }
+            else
+            {
+                MessageBox.Show(this, "Please enter a name");
+            }
+            
+        
+
+        }
+
+    }
+}
+
